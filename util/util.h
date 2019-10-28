908625ea9e48499a0773f44c5ab4e82184e2d322
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2019-10-28 11:36:58 -0500

    function definitions

--- util/util.h
+++ util/util.h
@@ -8,10 +8,12 @@ using namespace std;
 namespace vec_ops{
     //vector operations
     vector<double> cross(vector<double>, vector<double>);
-    vector<double> dot(vector<double>, vector<double>);
+    double dot(vector<double>, vector<double>);
     vector<double> add(vector<double>, vector<double>);
     vector<double> sub(vector<double>, vector<double>);
     vector<double> div(vector<double>, double scalar);
+    vector<double> resize(vector<double>, int);
+    bool agree(vector<double>, vector<double>);
 }
 
 namespace linalg{
@@ -27,7 +29,7 @@ namespace linalg{
                 elements = x;
             }
             
-            matrix inv();
+            Matrix inv();
 
             vector<vector<T>> get(){
                 return elements;
@@ -36,24 +38,58 @@ namespace linalg{
 
 }
 
-vector<double> vec_ops::cross(vector<double> x, vector<double> y){
-
+bool vec_ops::agree(vector<double> x, vector<double> y){
+    //makes sure vectors are of same dimension
+    return x.size() == y.size();
 }
 
-vector<double> vec_ops::dot(vector<double> x, vector<double> y){
+vector<double> vec_ops::cross(vector<double> x, vector<double> y){
+    
+}
 
+double vec_ops::dot(vector<double> x, vector<double> y){
+    if(!agree(x,y)){
+        throw -1;
+    }else{
+        double sum = 0;
+        for(unsigned i = 0; i < x.size(); i++){
+            sum+= x.at(i) * y.at(i);
+        }
+        return sum;
+    }
 }
 
 vector<double> vec_ops::add(vector<double> x, vector<double> y){
-
+    if(!agree(x,y)){
+        throw -1;
+    }else{
+        vector<double> resultant (x.size());
+        resultant.assign(resultant.size(), 0);
+        for(unsigned i = 0; i < x.size(); i++){
+            resultant.at(i) = x.at(i) + y.at(i);
+        }
+        return resultant;
+    }
 }
 
 vector<double> vec_ops::sub(vector<double> x, vector<double> y){
-
+    if(!agree(x,y)){
+        throw -1;
+    }else{
+        vector<double> resultant (x.size());
+        resultant.assign(resultant.size(), 0);
+        for(unsigned i = 0; i < x.size(); i++){
+            resultant.at(i) = x.at(i) - y.at(i);
+        }
+        return resultant;
+    }
 }
 
 vector<double> vec_ops::div(vector<double> x, double scalar){
-
+    for(unsigned i = 0; i<x.size(); i++){
+        x.at(i) = x.at(i)/scalar;
+    }
+    return x;
 }
 
 #endif // !UITL
