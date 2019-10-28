6bcf1af9b83755185ed0f5bae6247a87d6301257
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2019-10-27 21:51:17 -0500

    additions

--- util/util.h
+++ util/util.h
@@ -11,7 +11,7 @@ namespace vec_ops{
     vector<double> dot(vector<double>, vector<double>);
     vector<double> add(vector<double>, vector<double>);
     vector<double> sub(vector<double>, vector<double>);
-    vector<double> div(vector<double>, int scalar);
+    vector<double> div(vector<double>, double scalar);
 }
 
 namespace linalg{
@@ -19,16 +19,41 @@ namespace linalg{
     //matrixes are just gonna be grouped vectors in most cases I can think of?
 
     //will be double most of the time
-    template <class T> 
-    class matrix {
-        private:
-            vector<vector<T>> elements;
+    template <typename T> 
+    class Matrix {
+        vector<vector<T>> elements;
         public:
-            matrix(vector<vector<T>>);
+            Matrix(vector<vector<T>> x){
+                elements = x;
+            }
+            
             matrix inv();
-            vector<vector<T>> get();
+
+            vector<vector<T>> get(){
+                return elements;
+            }
     };
-    
+
+}
+
+vector<double> vec_ops::cross(vector<double> x, vector<double> y){
+
+}
+
+vector<double> vec_ops::dot(vector<double> x, vector<double> y){
+
+}
+
+vector<double> vec_ops::add(vector<double> x, vector<double> y){
+
+}
+
+vector<double> vec_ops::sub(vector<double> x, vector<double> y){
+
+}
+
+vector<double> vec_ops::div(vector<double> x, double scalar){
+
 }
 
 #endif // !UITL
