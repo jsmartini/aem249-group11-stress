3d2cf5fe70cf3fea3fa821da7e65784e6a45b9cb
jamescolvin331 <57075257+jamescolvin331@users.noreply.github.com>
2019-11-29 12:18:30 -0600

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,216 +1,234 @@
-#include <fstream>
-#include <vector>
 #include <iostream>
 #include <cmath>
-
 using namespace std;
-
-void caseone(double,double,double,double,double,double,double,double,double,double);
-void casetwo(double,double,double,double,double,double,double,double,double,double);
-void casethree(double,double,double,double,double,double,double,double,double,double);
-
-int main()
-{
-	double F = 0, Lc = 0, Lr = 0, I = 0, E = 0, d = 0, v = 0, G = 0, density = 0, maxnc = 0, maxnt = 0, maxs = 0;
-	char casenumber, material;
-	
-	cout << "This program takes a cylinder made out of a certain material and \n applies a force to it under a selected loading condition"<<endl;
-	cout << "\n\n Is the cylinder made out of steel(s), aluminum(a), or iron(i)" << endl;
-	cin >> material;
-	
-	while(material!='s'&& material!='a'&& material!='i')
-	{
-		cout << "\n Did not understand selection, enter 's' for steel, 'a' for aluminum, or 'i' for iron"<<endl;
-		cin >> material;
+int main(){
+	// First line of double variables are all of the inputed and reaction forces on the cylinder and set them equal to 0
+	double FRM=0, FRY=0, FRX=0, FRT=0, F1y=0, F1x=0, XF1=0, F2x=0, F2y=0, XF2=0, M1=0, XM1=0, M2=0, XM2=0, RDL1=0, XRL11=0, XRL12=0, RDL2=0, XRL21=0, XRL22=0, UDTL=0, TF1=0, XTF1=0, TF2=0, XTF2=0;
+	// Second line of double variables define the properties of the cylinder
+	double length, diameter, CSarea, torsionconstant, momentconstant, transverseconstant;
+	//third line of variables are used for orgainizing inputed values 
+	char answer;
+	cout<<"what is the diameter of the cylinder in meters"<<endl;
+	cin>>diameter;
+	while(diameter<=0){
+		cout<<"the diameter of the cylinder must be a positive non-zero value,\nenter another value for the diameter of the cylinder"<<endl;
+		cin>>diameter;
 	}
-	
-	if (material=='s')
-	{
-		cout << "\n The cylinder is made out of steel" << endl;
-		E = 12312;
-		v = 213123;
-		G = 2131231;
-		density = 231232;
-		maxnc = 123123;
-		maxnt = 1223123;
-		maxs = 4142141;
+	cout<<"what is the length of the cylinder in meters"<<endl;
+	cin>>length;
+	while (length<=0){
+		cout<<"the lenght of the cylinder must be a positive non-zero value,\nenter another value for the length of the cylinder in meters"<<endl;
+		cin>>length;
 	}
-	
-	else if(material=='a')
-	{
-		cout << "\n The cylinder is made out of aluminum" << endl;
-		E = 12312;
-		v = 213123;
-		G = 2131231;
-		density = 231232;
-		maxnc = 123123;
-		maxnt = 1223123;
-		maxs = 4142141;
+	CSarea = diameter*diameter*M_PI*0.25;
+	momentconstant = 32/(M_PI*pow(diameter,3));
+	torsionconstant = 16/(M_PI*pow(diameter,3));
+	transverseconstant = 4/(3*M_PI*pow(diameter/2,2));
+	cout<<"Is there a concentrated force acting on the cylinder(y,n)"<<endl;
+	cin>>answer;
+	while(answer!= 'y' && answer != 'n'){
+		cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+		cin>>answer;
 	}
-	
-	else if(material=='i')
-	{
-		cout << "\n The cylinder is made out of iron" << endl;
-		E = 12312;
-		v = 213123;
-		G = 2131231;
-		density = 231232;
-		maxnc = 123123;
-		maxnt = 1223123;
-		maxs = 4142141;
+	if (answer== 'y'){         // collecting inputs of concentrated forces on the cylinder
+		cout<<"what is the 'x' value of the concentrated load in Newtons.  positive values are pointed away from the wall support @ x=0"<<endl;
+		cin>>F1x;
+		cout<<"what is the 'y' value of the concentrated load in Newtons.  positive values are pointed up in the oppisite direction of gravity"<<endl;
+		cin>>F1y;
+		cout<<"at what x value in meters along the cylinder is the concentrated load located?"<<endl;
+		cin>>XF1;
+		while (XF1<=0 or XF1>length){
+			cout<<"the x value for the location of the concentrated load on the cylinder must be a positive non-zero value that is less than or equal to the lenth of the cylinder,\nenter another value"<<endl;
+			cin>>XF1;
+		}
+		cout<<"is there another concentrated load acting on the cylinder(y,n)"<<endl;
+		cin>>answer;
+		while(answer!= 'y' && answer != 'n'){
+			cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+			cin>>answer;
+		}
+		if (answer== 'y'){
+			cout<<"what is the value of the 'x' value of the concentrated load in Newtons.  positive values are pointed away from the wall support or first pin located @ x=0"<<endl;
+			cin>>F2x;
+			cout<<"what is the value of the 'y' value of the concentrated load in Newtons.  positive values are pointed up in the oppisite direction of gravity"<<endl;
+			cin>>F2y;
+			cout<<"at what x value in meters along the cylinder is the concentrated load located?"<<endl;
+			cin>>XF2;
+			while (XF2<=0 or XF2>length){
+				cout<<"the x value for the location of the concentrated load on the cylinder must be a positive non-zero value that is less than or equal to the lenth of the cylinder,\nenter another value"<<endl;
+				cin>>XF2;
+			}
+		}
 	}
-	
-	cout << "\n\n What is the diameter of the cylinder in meters" << endl;
-	cin >> d;
-	
-	while(d<=0)
-	{
-		cout << "\n The diameter must be a positive value larger than 0,\n enter a value for the diameter in meters" << endl;
-		cin >> d;
+	cout<<"is there a concentrated moment force about the 'z' axis acting on the cylinder?(y,n)"<<endl;
+	cin>>answer;
+	while(answer!= 'y' && answer != 'n'){
+		cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+		cin>>answer;
 	}
-	
-	cout << "\n\n What is the length of the cylinder in meters" << endl;
-	cin >> Lc;
-	
-	while(Lc<=0)
-	{
-		cout << "\n The length of the cylinder must be a positive value larger than 0,\nenter a value for the length of the cylinder in meters" << endl;
-		cin >> Lc;
+	if(answer== 'y'){    //collecting values for concentrated moments on the cylinder
+		cout<<"what is the value of the concentrated moment in Newton meters? positive values are rotating counterclockwise"<<endl;
+		cin>>M1;
+		cout<<"at what x value in meters along the cylinder is the conentrated moment located?"<<endl;
+		cin>>XM1;
+		while (XM1<=0 or XM1>length){
+			cout<<"the x value for the location of the concentrated load on the cylinder must be a positive non-zero value that is less than or equal to the lenth of the cylinder,\nenter another value"<<endl;
+			cin>>XM1;
+		}
+		cout<<"is there another concentrated moment force about the 'z' axis acting on the cylinder?(y,n)"<<endl;
+		cin>>answer;
+		while(answer!= 'y' && answer != 'n'){
+			cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+			cin>>answer;
+		}
+		if(answer== 'y'){
+			cout<<"what is the value of the concentrated moment in Newton meters? positive values are rotating counterclockwise"<<endl;
+			cin>>M2;
+			cout<<"at what x value in meters along the cylinder is the conentrated moment located?"<<endl;
+			cin>>XM2;
+			while (XM2<=0 or XM2>length){
+				cout<<"the x value for the location of the concentrated load on the cylinder must be a positive non-zero value that is less than or equal to the lenth of the cylinder,\nenter another value"<<endl;
+				cin>>XM2;
+			}
+		}
 	}
-	
-	cout << "\n\n What is the force being applied in newtons" << endl;
-	cin >> F;
-	
-	while(F<0)
-	{
-		cout << "The force value must be a positive value,\nenter a positive value for the force in newtons"<<endl;
-		cin >> F;
+	cout<<"is there a rectangualr distributed load acting on the cylinder(y,n)"<<endl;
+	cin>>answer;
+	while(answer!= 'y' && answer != 'n'){
+		cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+		cin>>answer;
 	}
-	
-	cout << "\n\n Is the cylinder in a torsion case(t), horizontal loading case(h), or vertical loading case(v)" << endl;
-	cin >> casenumber;
-	
-	while(casenumber!='t'&&casenumber!='h'&&casenumber!='v')
-	{
-		cout << "\n Did not understand input for selection, enter 't' for a torion case, 'h' for a horizontal loading case, or 'v' for a vertical loading case" << endl;
-		cin >> casenumber;
+	if(answer=='y'){               //collecting inputs for the distributed loads
+		cout<<"what is the value of the distributed load in newtons per meter"<<endl;
+		cin>>RDL1;
+		cout<<"at what x value in meters does the distributed load begin"<<endl;
+		cin>>XRL11;
+		cout<<"at what x value in meters does the distributed load end"<<endl;
+		cin>>XRL12;
+		cout<<"is there another rectangular distributed load acting on the cylinder(y,n)"<<endl;
+		while(answer!= 'y' && answer != 'n'){
+			cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+			cin>>answer;
+		}
+		if(answer=='y'){
+			cout<<"what is the value of the distributed load in newtons per meter"<<endl;
+			cin>>RDL2;
+			cout<<"at what x value in meters does the distributed load begin"<<endl;
+			cin>>XRL21;
+			cout<<"at what x value in meters does the distributed load end"<<endl;
+			cin>>XRL22;
+		}
 	}
-	
-	cout << "\n\n";
-	
-	if(casenumber=='t')
-	{
-		caseone(F,d,Lc,density,E,v,G,maxnc,maxnt,maxs);
+	cout<<"is there a torsion force about the 'x' axis acting on the cylinder(y,n)"<<endl;
+	cin>>answer;
+	while(answer!= 'y' && answer != 'n'){
+		cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+		cin>>answer;
 	}
-	
-	else if(casenumber=='h')
-	{
-		casetwo(F,d,Lc,density,E,v,G,maxnc,maxnt,maxs);
+	if(answer=='y'){
+		cout<<"what is the value of the torsion force about the 'x' axis in Newton meters? positive values are clockwise"<<endl;
+		cin>>TF1;
+		cout<<"at what x value in meters along the cylinder is the torsion force acting?"<<endl;
+		cin>>XTF1;
+		cout<<"is there another torsion force about the 'x' axis acting on the cylinder?(y,n)"<<endl;
+		cin>>answer;
+		while(answer!= 'y' && answer != 'n'){
+			cout<<"did not understand input, enter 'y' for yes and 'n' for no"<<endl;
+			cin>>answer;
+		}
+		if(answer=='y'){
+			cout<<"what is the value of the torsion force about the 'x' axis in Newton meters? positive values are clockwise"<<endl;
+			cin>>TF2;
+			cout<<"at what x value in meters along the cylinder is the torsion force acting?"<<endl;
+			cin>>XTF2;
+		}
 	}
-	
-	else if(casenumber=='v')
-	{
-		casethree(F,d,Lc,density,E,v,G,maxnc,maxnt,maxs);
+	FRT=TF1+TF2;
+	FRX=(F1x+F2x)*(0-1);
+	FRY=(F1y+F2y+(RDL1*(XRL12-XRL11))+(RDL2*(XRL22-XRL21)))*(0-1);
+	FRM=((F1y*XF1)+(F2y*XF2)+((RDL1*(XRL12-XRL11))*(XRL11+((XRL12-XRL11)/2)))+((RDL2*(XRL22-XRL21))*(XRL11+((XRL12-XRL11)/2))))*(0-1);
+	double ABNS, ABNSM, ABSS, ABSSM, BBNS, BBNSM, BBSS, BBSSM, maxstress, maxstrain;
+	double localVF, localMF, localTF, localAF;
+	for(double localx; localx<=length;localx+=.1){
+		localVF=FRY;
+		localMF=FRM+(FRY*(0-localx));
+		localTF=FRT;
+		localAF=FRX;
+		if(XF1<=localx){
+			localVF=+F1y;
+			localMF=+(F1y*(XF1-localx));
+			localAF=+F1x;
+		}
+		if(XF2<=localx){
+			localVF=+F2y;
+			localMF=+(F2y*(XF2-localx));
+			localAF=+F2x;
+		}
+		if(XTF1<=localx){
+			localTF=+TF1;
+		}
+		if(XTF2<=localx){
+			localTF=+TF2;
+		}
+		if(XRL11<=localx){
+			if(XRL12<=localx){
+				localMF=+(RDL1*(XRL12-XRL11)*((XRL11+((XRL12-XRL11)/2))-localx));
+				localVF=+(RDL1*(XRL12-XRL11));
+			}
+			else{
+				localMF=+(RDL1*(localx-XRL11)*((XRL11+((localx-XRL11)/2))-localx));
+				localVF=+(RDL1*(localx-XRL11));
+			}
+		}
+		if(XRL21<=localx){
+			if(XRL22<=localx){
+				localMF=+(RDL2*(XRL22-XRL21)*((XRL21+((XRL22-XRL21)/2))-localx));
+				localVF=+(RDL2*(XRL22-XRL21));
+			}
+			else{
+				localMF=+(RDL2*(localx-XRL21)*((XRL21+((localx-XRL21)/2))-localx));
+				localVF=+(RDL2*(localx-XRL21));
+			}
+		}
+		if(XM1<=localx){
+			localMF=+M1;
+		}
+		if(XM2<=localx){
+			localMF=+M2;
+		}
+		if(localAF<0){
+			localAF=localAF*-1;
+		}
+		if(localVF<0){
+			localVF=localVF*-1;
+		}
+		if(localMF<0){
+			localMF=localMF*-1;
+		}
+		if(localTF<0){
+			localTF=localTF*-1;
+		}
+		ABNS=((localAF)/CSarea)+(localMF*momentconstant);
+		ABSS=localTF*torsionconstant;
+		BBNS=localAF/CSarea;
+		BBSS=(localTF*torsionconstant)+(localVF*transverseconstant);
+		ABNSM=(ABNS/2)+sqrt(pow(ABNS/2,2)+pow(ABSS,2));
+		ABSSM=sqrt(pow(ABNS/2,2)+pow(ABSS,2));
+		BBNSM=(BBNS/2)+sqrt(pow(BBNS/2,2)+pow(BBSS,2));
+		BBSSM=sqrt(pow(BBNS/2,2)+pow(BBSS,2));
+		if(ABNSM>maxstress){
+			maxstress=ABNSM;
+		}
+		if(ABSSM>maxstrain){
+			maxstrain=ABSSM;
+		}
+		if(BBNSM>maxstress){
+			maxstress=ABNSM;
+		}
+		if(BBSSM>maxstrain){
+			maxstrain=ABSSM;
+		}
 	}
-	
-	return 0;
-}
-
-void caseone(double F, double d, double Lc, double density, double E, double v, double G, double maxnc, double maxnt, double maxs)
-{
-	cout << "This is the torsion case" << endl;
-	
-	double T = 0, M = 0, I = 0, Lr = 0, ns = 0, ss = 0;
-	cout << "What is the length of the rod connected at the end of the cylinder \n measured from the center of the cylinder \n to force vector in meters" << endl;
-	cin >> Lr;
-	
-	while(Lr<=0)
-	{
-		cout << "\n The length of the rod must be a positive value larger than 0, \n enter a value for the length of the rod in meters" << endl;
-		cin >> Lr;
-	}
-	
-	T = F * Lr;
-	M = F * Lc;
-	I = M_PI * pow(d,4) / 32;
-	ns = M * d / 2 / I;
-	ss = T * d / 2 / I;
-	
-	if(ns>maxnc)
-	{
-		cout << "The cylinder fails due to compressive normal stress" << endl;
-	}
-	else if(ns>maxnt)
-	{
-		cout << "The cylinder fails due to tensile normal stress" << endl;
-	}
-	else if(ss>maxs)
-	{
-		cout << "The cylinder fails due to shear stress" << endl;
-	}
-	else
-	{
-		cout << "The part will not fail,\nthe max normal stress experienced is " << ns << "\n and the max ss experienced is " << ss << endl;
-	}
-}
-void casetwo(double F, double d, double Lc, double density, double E, double v, double G, double maxnc, double maxnt, double maxs)
-{
-	cout << "This is the horizontal loading case" << endl;
-	
-	double w = 0, I = 0, M = 0, r = 0, ns = 0, ss = 0;
-	
-	I = M_PI * pow(d,4) / 32;
-	r = d / 2;
-	w = density * M_PI * d * d * Lc / 4;
-	M= (F * Lc) + (w * Lc / 2);
-	ns = M * r / I;
-	ss = (F + w) / (M_PI * d * d / 4);
-	
-	if(ns>maxnc)
-	{
-		cout << "The cylinder will fail due to normal compressive stress" << endl;
-	}
-	
-	if(ns>maxnt)
-	{
-		cout << "The cylinder will fail due to normal tensile stress" << endl;
-	}
-	
-	if(ss>maxs)
-	{
-		cout << "The cylinder will fail due to shear stress" << endl;
-	}
-}
-void casethree(double F, double d, double Lc, double density, double E, double v, double G, double maxnc, double maxnt, double maxs)
-{
-	cout << "This is the vertical loading condition" << endl;
-	
-	double I = 0, ns = 0, ss = 0, maxb = 0, Ac = 0;
-	Ac = M_PI * d * d/4;
-	I = M_PI * pow(d,4) / 32;
-	maxb = M_PI * M_PI * E * I / 4 / Lc / Lc;
-	ns = F / Ac;
-	ss = ns / 2;
-	
-	if(maxb<F)
-	{
-		cout << "The cylinder will buckle under the force" << endl;
-	}
-	
-	if(ns>maxnc)
-	{
-		cout << "The cylinder fails due to compressive forces" << endl;
-	}
-	
-	if(ss<=maxs&&maxb<=F&&ns<=maxnc)
-	{
-		cout << "The cylinder will not buckle under the Force applied" << endl;
-		cout << "The max normal stress acting on the cylinder is " << ns << endl;
-		cout << "The max shear stress acting on the cylinder is " << ss << endl;
-	}
-	
-	if(ss>maxs)
-	{
-		cout << "The cylinder will fail due to the shear stress" << endl;
-	}		
+	cout<<"The max stress in the cylinder is "<<maxstress<<"Pa"<<endl;
+	cout<<"The max strain in the cylinder is "<<maxstrain<<"Pa"<<endl;
 }
