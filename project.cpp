c413919c32ec9bc88fd85e7fd6ff9ab47c6b29d6
jamescolvin331 <57075257+jamescolvin331@users.noreply.github.com>
2019-12-01 19:27:51 -0600

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <math.h>
-
-//#define M_PI 3.14
+//math.h is used to define M_PI as 3.14159... and for the power and squareroot functions
 
 using namespace std;
 
 int main()
 {
-	//First line of double variables are all of the inputed and reaction forces on the cylinder and set them equal to 0.
-	double FRM = 0, FRY = 0, FRX = 0, FRT = 0, F1y = 0, F1x = 0, XF1 = 0, F2x = 0, F2y = 0, XF2 = 0, M1 = 0, XM1 = 0, M2 = 0, XM2 = 0, RDL1 = 0, XRL11 = 0, XRL12 = 0, RDL2 = 0, XRL21 = 0, XRL22 = 0, UDTL = 0, TF1 = 0, XTF1 = 0, TF2 = 0, XTF2 = 0;
+	double FRM = 0, FRY = 0, FRX = 0, FRT = 0, F1y = 0, F1x = 0, XF1 = 0, F2x = 0, F2y = 0, XF2 = 0, M1 = 0, XM1 = 0, M2 = 0, XM2 = 0, RDL1 = 0, XRL11 = 0, XRL12 = 0, RDL2 = 0, XRL21 = 0, XRL22 = 0, TF1 = 0, XTF1 = 0, TF2 = 0, XTF2 = 0;
 	
-	//Second line of double variables define the properties of the cylinder.
+	//FRM, FRY, FRX, and FRT are the reaction forces at the wall broken down into the moment about the z axis, y axis, x axis, and a reaction torsion force about the x axis
+	//F1x and F1y are the 1st concentrated force x and y components
+	//F2x and F2y are the 2nd concentrated force x and y components
+	//XF1 and XF2 are the locations that the 2 concentrated force act on the cylinder
+	//M1 and M2 are the concentrated moments about the z axis acting on the cylinder
+	//XM1 and XM2 are the locations where the concentrated moments are acting on the cylinder
+	//TF1 and TF2 are the torsion forces acting on the cylinder at locations XTF1 and XTF2 respectfully
+	//RDL1 and RDL2 are the rectangular distributed loads
+	//XRL11 and XRL12 are the starting and ending locations for the first rectangular distributed load
+	//XRL21 and XRL22 are the starting and ending locations for the second rectangular distributed load
+	
 	double length = 0, diameter = 0, CSarea = 0, torsionconstant = 0, momentconstant = 0, transverseconstant = 0;
 	
-	//Third line of variables are used for orgainizing inputed values.
+	//I used the answer variable as a way of asking the user what forces are present on the cylinder
+	
 	char answer;
 	
 	cout << "What is the diameter of the cylinder in meters?"<<endl; //Get values for the dimentions of the cylinder.
@@ -35,6 +44,9 @@ int main()
 	}
 	
 	CSarea = diameter * diameter * M_PI * 0.25;  //Solve for constants based on the diminsions of the cylinder.
+	
+	//these constants are based on the initial formulas for moment stresses, torsion train, and transverse shear stress
+	//I calcualted a portion of the formulas as constants to make solving for the stresses and strains less complicated in the for loop
 	
 	momentconstant = 32 / (M_PI * pow(diameter, 3));
 	
@@ -282,8 +294,9 @@ int main()
 		}
 	}
 	
-	//Calculating reaction forces at the wall.
-	FRT = TF1 + TF2;
+	//Calculating reaction forces at the wall, i multiply each by (0-1) to swap the sign for the reaction force
+	
+	FRT = (TF1 + TF2) * (0 - 1);
 	
 	FRX = (F1x + F2x) * (0 - 1);
 	
@@ -291,19 +304,30 @@ int main()
 	
 	FRM = ((F1y * XF1) + (F2y * XF2) + ((RDL1 * (XRL12 - XRL11)) * (XRL11 + ((XRL12 - XRL11) / 2))) + ((RDL2 * (XRL22 - XRL21)) * (XRL11 + ((XRL12 - XRL11) / 2)))) * (0 - 1);
 	
-	double ABNS = 0, ABNSM = 0, ABSS = 0, ABSSM = 0, BBNS = 0, BBNSM = 0, BBSS = 0, BBSSM = 0, maxstress = 0, maxstrain = 0, localVF = 0, localMF = 0, localTF = 0, localAF = 0, maxstressxlocation = 0, maxstrainxlocation = 0, maxstresslocation = 0, maxstrainlocation = 0;
+	double ABNS = 0, ABNSM = 0, ABSS = 0, ABSSM = 0, BBNS = 0, BBNSM = 0, BBSS = 0, BBSSM = 0, maxnormal = 0, maxshear = 0, localVF = 0, localMF = 0, localTF = 0, localAF = 0, maxnormalxlocation = 0, maxshearxlocation = 0, maxnormallocation = 0, maxshearlocation = 0;
 	
-	for(double localx = 0; localx <= length; localx += .01) //For loop calulates the stresses and strains in the cylinder from 0 to the end of cylinder in increments of .01 meters.
+	for(double localx = 0; localx <= length; localx += (length/100000))
 	{
+		
+		//the local variables are the forces and moments found at each location
+		//the for loop starts at x=0 and goes till x=length which would be end of the cylinder
+		//at each location or "localx" the shear force(localVF), axial force(localAF), moment about
+		// the z-axis(localMF), and torsion force about the x-axis(localTF) are calulated
+		
 		localVF = FRY;
 		localMF = FRM + (FRY * (0 - localx));
 		localTF = FRT;
 		localAF = FRX;
 		
-		if(XF1 <= localx) //As x increases any conentrated force, concentrated moment, distributed load, or torsion force.
+		//the if statements are there to determine if the force or moment is being used yet
+		//since the cylinder is in static equalibrum at any point the forces and moments on the left
+		//equal the ones on the right so these if statement find which forces and moments are acting on the left hand
+		//side of localx value and sum them up 
+		
+		if(XF1 <= localx)
 		{
-			localVF += F1y; //That is acting at a point that is equal to or less that the x value is used to calcuate.
-			localMF += (F1y * (XF1 - localx)); //The stress and strain at that point.
+			localVF += F1y;
+			localMF += (F1y * (XF1 - localx)); 
 			localAF += F1x;
 		}
 		
@@ -364,6 +388,8 @@ int main()
 			localMF += M2;
 		}
 		
+		//I set all local forces positive to help find the maximum values of normal and shear stress
+		
 		if(localAF < 0)
 		{
 			localAF = localAF * (0 - 1);
@@ -384,6 +410,12 @@ int main()
 			localTF = localTF * (0 - 1);
 		}
 		
+		//here is where i use the locally calulated forces and moments to find the stress and strains
+		//the two locations where a maximum stress or strain will occur is either on the top and bottom or the sides of the cylinder
+		//ABNS is the normal stress located at the top or bottom of the cylinder at each localx
+		//BBNS is the normal stress located on the sides of the cylinder at each localx
+		//ABSS and BBSS are the shear stresses at either the top or bottom or sides of the cylinder at each localx
+		
 		ABNS = ((localAF) / CSarea) + (localMF * momentconstant);
 		
 		ABSS = localTF * torsionconstant;
@@ -391,6 +423,12 @@ int main()
 		BBNS = localAF / CSarea;
 		
 		BBSS = (localTF * torsionconstant) + (localVF * transverseconstant);
+		
+		//after the normal stress and shear stress are found at each localx the max principle stress and max shear stress are found
+		//the maxes are caluated using mohrs circle
+		//ABNSM is the max normal stress on the top or bottom
+		//ABSSM is the max shear stress on the top or bottom
+		//BBNSM and BBSSM are the max normal and shear stress and the sides
 		
 		ABNSM = (ABNS / 2) + sqrt(pow(ABNS / 2, 2) + pow(ABSS, 2));
 		
@@ -400,36 +438,40 @@ int main()
 		
 		BBSSM = sqrt(pow(BBNS / 2, 2) + pow(BBSS, 2));
 		
-		if(ABNSM > maxstress)
+		//these if statements determine if the localx max shear and nomal strains are larger than the previous maxes
+		// if they are they replace them as the new maxnormal and maxshear
+		
+		if(ABNSM > maxnormal)
 		{
-			maxstress = ABNSM;
-			maxstressxlocation = localx;
-			maxstresslocation = 1;
+			maxnormal = ABNSM;
+			maxnormalxlocation = localx;
+			maxnormallocation = 1;
 		}
 		
-		if(ABSSM > maxstrain)
+		if(ABSSM > maxshear)
 		{
-			maxstrain = ABSSM;
-			maxstrainxlocation = localx;
-			maxstrainlocation = 1;
+			maxshear = ABSSM;
+			maxshearxlocation = localx;
+			maxshearlocation = 1;
 		}
 		
-		if(BBNSM > maxstress)
+		if(BBNSM > maxnormal)
 		{
-			maxstress = ABNSM;
-			maxstressxlocation = localx;
-			maxstresslocation = 2;
+			maxnormal = ABNSM;
+			maxnormalxlocation = localx;
+			maxnormallocation = 2;
 		}
 		
-		if(BBSSM > maxstrain)
+		if(BBSSM > maxshear)
 		{
-			maxstrain = ABSSM;
-			maxstrainxlocation = localx;
-			maxstrainlocation = 2;
+			maxshear = ABSSM;
+			maxshearxlocation = localx;
+			maxshearlocation = 2;
 		}
 	}                 
 
 	//Outputs important information that could to used to verify that the code is working as well as determine where it went wrong.
+	
 	cout << "The reaction force at the wall in the 'y' direction is " << FRY << "N." << endl;
 	
 	cout << "The reaction force at the wall in the 'x' direction is " << FRX << "N." << endl;
@@ -438,27 +480,27 @@ int main()
 	
 	cout << "The reaction moment force about the 'z' axis at the wall is " << FRM << "Nm." << endl;
 	
-	cout << "The max stress in the cylinder is " << maxstress << "Pa at " << maxstressxlocation << "meters from the wall." << endl;
+	cout << "The max normal stress in the cylinder is " << maxnormal << "Pa at " << maxnormalxlocation << " meters from the wall." << endl;
 	
-	cout << "The max strain in the cylinder is " << maxstrain << "Pa at " << maxstrainxlocation << "meters from the wall." << endl;
+	cout << "The max shear stress in the cylinder is " << maxshear << "Pa at " << maxshearxlocation << " meters from the wall." << endl;
 	
-	if(maxstresslocation == 1)
+	if(maxnormallocation == 1)
 	{
-		cout << "The max stress is located on the top or bottom of the cylinder." << endl;	
+		cout << "The max normal stress is located on the top or bottom of the cylinder." << endl;	
 	}
 	
-	if(maxstresslocation == 2)
+	if(maxnormallocation == 2)
 	{
-		cout << "The max strain is located on the left or right side of the cylinder." << endl;	
+		cout << "The max normal stress is located on the left or right side of the cylinder." << endl;	
 	}
 	
-	if(maxstrainlocation == 1)
+	if(maxshearlocation == 1)
 	{
-		cout << "The max strain is located on the top or bottom of the cylinder." << endl;	
+		cout << "The max shear stress is located on the top or bottom of the cylinder." << endl;	
 	}
 	
-	if(maxstrainlocation == 2)
+	if(maxshearlocation == 2)
 	{
-		cout << "The max strain is located on the left or right side of the cylinder" << endl;	
+		cout << "The max shear stress is located on the left or right side of the cylinder" << endl;	
 	}
 }
